cdb8b850ad054a04204230be5743bc5d62d597a0
obada <obadatumah19@gmail.com>
2020-03-16 15:50:52 +0200

    solving question three

---
+++ q3.js
@@ -0,0 +1,68 @@
+'use strict'
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+// this code for put the date.
+var fullDate = new Date();
+var today = fullDate.getDate();
+var month = months[fullDate.getMonth()];
+var year = fullDate.getFullYear();
+var dateToday = document.getElementById('dateP');
+dateToday.textContent = `${month} ${today} ${year}`;
+
+
+
+
+var resultList = document.getElementById('orderList');
+var theList = document.createElement('ol');
+resultList.appendChild(theList);
+
+var arrayOfObjects = [];
+
+function activities(act, actDate) {
+    this.act = act;
+    this.actDate = actDate;
+
+    arrayOfObjects.push(this);
+}
+var submitt = document.getElementById('submitResult');
+submitt.addEventListener('click', function (event) {
+    event.preventDefault();
+    
+    
+    var input1 = document.getElementById('doneToday').value;
+    var input2 = document.getElementById('dateSelected').value;
+    console.log(input1);
+    
+
+    var liE = document.createElement('li');
+    theList.appendChild(liE);
+    
+    liE.textContent = `${input1} at ${input2}`;
+
+    new activities(input1, input2);
+    setData();
+   
+
+
+});
+
+var newArray = [];
+function setData() {
+    var userData = JSON.stringify(arrayOfObjects);
+    localStorage.setItem('obada', userData);
+}
+
+function getData() {
+    var userData = localStorage.getItem('obada');
+    if (userData) {
+        newArray = JSON.parse(userData);
+
+        
+        for(var i=0; i<newArray.length;i++){
+        var liE = document.createElement('li');
+        theList.appendChild(liE);
+        liE.textContent = `${newArray[i].act} at ${newArray[i].actDate}`;}
+    }
+}
+getData();
